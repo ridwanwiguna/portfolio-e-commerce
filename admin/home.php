199bddb65f7e99975ed438f2be3b017f0bb38ff9
Ridwan Wiguna <wigunaridwan6@gmail.com>
2021-08-28 11:14:12 +0700

    Commit Pertama

---
+++ admin/home.php
@@ -0,0 +1,15 @@
+<h3><marquee>Selamat Datang Admin Ladies Store Jambi</marquee></h3> 
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12"> 
+                     <tr>
+    <td bgcolor="#FFFFFF">
+      <h5><img src="assets/img/ladies2.jpg" width="900" height="400" border="3" align="center"  />.  </h5>
+      <h5>
+  </h5></td>
+  </tr>  
+                        
+                    </div>
+                </div> 
+            </div>             
+                 
---
+++ template/footer.php
@@ -0,0 +1,32 @@
+<footer class="bg-dark text-white p-5">
+            <div class="row">
+              <div class="col-md-3">
+                <h5>LAYANAN PELANGGAN</h5>
+                <ul>
+                <li><a href="carapembelian.php">Cara pembelian</a></li>
+                  <li><a href="pembayaran.php">Konfirmasi Pembelian</a></li>
+                  
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h5>TENTANG KAMI</h5>
+                <p>Ladies Store adalah Toko khusus Fashion Wanita, yang bertempat di alamat Jl Ir H juanda , Mayang . Kel simpang 3 sipin , kec kotabaru jambi </p>
+              </div>
+              <div class="col-md-3">
+                <h5>MITRA KERJA SAMA</h5>
+                <ul>
+                  
+                  <li>JNE</li>
+                </ul>
+              </div>
+              <div class="col-md-3">
+                <h5>HUBUNGI KAMI</h5>
+                <ul>
+                  <li>Whatsapp/Telp</li>
+                  <li>081281741591</li>
+                  
+                </ul>
+              </div>
+              
+            </div>
+          </footer>
